f6a9213d6baa077241cf34a2696d874dc9f32a3e
Autre TechLab <AutreTechLab@gmail.com>
2021-05-02 15:54:19 +0200

    Cognitio config

--- main/js/config.js
+++ main/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_axWRbsGa3', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '6v9epc24o4q1s8bp4ma2e0oi9o', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'eu-central-1_LZXd6oFRY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4m1b7n1gsh3tb55mu71rr7n36m', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://www.autretechlab.cloud/main' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
